47b28d718ab434b9e99e16623ef3f72a83a8017b
Shulik <kachko,shulik@gmail.com>
2021-05-08 16:17:06 +0300

    LDA done, starting SVM

--- iml_ex3/models.py
+++ iml_ex3/models.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.svm import SVC
 
 
 class Perceptron:
@@ -13,7 +14,6 @@ class Perceptron:
         and stores the trained model in self.model
         :param X: a numpy array of size m x d.
         :param y: a binary vector of size m.
-        :return:
         """
         self.weights = np.zeroes(shape=(X.shape[1]))  # init column vec of zeroes
         hom_X = np.hstack(np.ones((X.shape[0], 1), X))
@@ -24,7 +24,7 @@ class Perceptron:
                     exists = True  # update flag
                     self.weights = self.weights + y[i] * hom_X[i]
             if not exists:
-                return self.weights
+                break
 
     def predict(self, X):
         """
@@ -36,18 +36,52 @@ class Perceptron:
         return np.sign(hom_X @ self.weights)
 
     def score(self, X, y):
+        """
+        :param X: unlabeled test set given as numpy array of dim mXd
+        :param y: numpy array of dim m - the true labeles
+        :return: a dictionary with the following fields:
+        num_samples: number of samples in test set.
+        error: error (misclassification rate)
+        accuracy: number of correct classifications out of all predictions
+        FPR: false positive rate
+        TPR: true positive rate
+        precision:
+        specificity:
+        """
         pass
 
 
+# noinspection PyTypeChecker
 class LDA:
     def __init__(self):
-        pass
+        self.deltas = []
 
     def fit(self, X, y):
-        pass
+        """
+        Given a training set, this method learns the parameters of the model
+        and stores the trained model in self.model
+        :param X: a numpy array of size m x d.
+        :param y: a binary vector of size m.
+        """
+        for _y in [1, -1]:
+            prob = np.sum(y == _y) / len(y)  # probability to get val _y
+            ln_P = np.log(prob)
+            mean_vec = np.array([np.mean(x_i) for x_i in X[y == _y]])
+            inv_sig = np.pinv(np.cov(X))
+
+            delta = [(x.T @ inv_sig @ mean_vec - 0.5 * mean_vec.T @ inv_sig @ mean_vec + ln_P) for x in X]
+            self.deltas.append(delta)
 
     def predict(self, X):
-        pass
+        """
+        uses the fitted parameters of the model to predict over a given dataset.
+        :param X: numpy array of dimension mxd
+        :return: the prediction of the trained model
+        """
+        del_1, del_min1 = 1., -1.
+        y_hat = np.array(
+            [{0: 1., 1: -1.}[np.argmax([self.deltas[0][i], self.deltas[1][i]])] for i in range(X.shape[0])])
+        return y_hat
 
     def score(self, X, y):
         pass

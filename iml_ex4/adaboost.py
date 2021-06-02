3b14c5ff84a1ffe8fd6f13b135ec1417c1c25416
Shulik <kachko,shulik@gmail.com>
2021-06-02 20:30:36 +0300

    q13 done

--- iml_ex4/adaboost.py
+++ iml_ex4/adaboost.py
@@ -8,6 +8,10 @@ Skeleton for the AdaBoost classifier.
 """
 import numpy as np
 
+from ex4_tools import *
+
+NO_NOISE = 0
+
 
 class AdaBoost(object):
 
@@ -39,7 +43,7 @@ class AdaBoost(object):
             y_hat = (self.h[i]).predict(X)
             err_t = np.sum(D[y != y_hat])
             self.w[i] = 0.5 * np.log((1 / err_t) - 1)
-            D = np.multiply(D, np.exp(-y * self.w[i] * self.h[i]))  # update sample weights
+            D = np.multiply(D, np.exp(-y * self.w[i] * y_hat))  # update sample weights
             D = D / np.sum(D)
         return D
 
@@ -65,5 +69,33 @@ class AdaBoost(object):
         :param max_t: integer < self.T: the number of classifiers to use for the classification
         :return: error : the ratio of the wrong predictions when predict only with max_t weak learners (float)
         """
-        wrong = sum(np.ones(y[y != self.predict(X, max_t)]))  # number of wrong classifications
+        pred_vec = self.predict(X, max_t)
+        wrong = sum(np.ones(y[y != pred_vec].shape))  # number of wrong classifications
         return wrong / len(y)
+
+
+def q_13(m=5000, T=500, m_test=200):
+    # create data and train model
+    X_train, y_train = generate_data(m, NO_NOISE)
+    X_test, y_test = generate_data(m_test, NO_NOISE)
+    ab = AdaBoost(DecisionStump, T)
+    ab.train(X_train, y_train)
+    test_err, train_err = [], []
+
+    # get error for each
+    for i in range(0, T):
+        train_err.append(ab.error(X_train, y_train, i))
+        test_err.append(ab.error(X_test, y_test, i))
+    T_range = np.arange(T)
+
+    # plots
+    plt.plot(T_range, train_err, label='Train Error')
+    plt.plot(T_range, test_err, label='Test Error')
+    plt.legend()
+    plt.grid(True)
+    plt.title("Error vs. T")
+
+
+
+if __name__ == '__main__':
+    pass

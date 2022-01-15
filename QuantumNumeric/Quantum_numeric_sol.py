3d4880731adf903fc53b631a0084e23e077c4c22
Shulik <kachko,shulik@gmail.com>
2022-01-15 20:10:20 +0200

    Q4 done, starting Q5

--- QuantumNumeric/Quantum_numeric_sol.py
+++ QuantumNumeric/Quantum_numeric_sol.py
@@ -1,6 +1,10 @@
 import numpy as np
+from sympy.physics.hydrogen import E_nl, R_nl
+from scipy.misc import derivative
+from numpy import linalg as LA
 from matplotlib import pyplot as plt
-from scipy.special import genlaguerre as L
+from scipy.special.orthogonal import genlaguerre as L
+
 from sympy import *
 
 
@@ -11,11 +15,12 @@ def calc_R_nl(r, n, l):
     :param n: int - quantum number of energy level
     :param l: int - quantum number of angular momentum, must be < n
     """
-    rho = float(2 * r)
-    poly_deg = n - l - 1
-    in_sqrt = 8 * ((np.math.factorial(poly_deg)) / (np.math.factorial(n + l + 1)))
-    outer = (rho ** l) * L(poly_deg, 2 * l + 2)(np.around(rho, 20)) * np.exp(-rho / 2)
-    return np.sqrt(in_sqrt) * outer
+    # rho = float(2 * r)
+    # poly_deg = n - l - 1
+    # in_sqrt = 8 * ((np.math.factorial(poly_deg)) / (np.math.factorial(n + l + 1)))
+    # outer = (rho ** l) * L(poly_deg, 2 * l + 2)(np.around(rho, 20)) * np.exp(-rho / 2)
+    # return np.sqrt(in_sqrt) * outer
+    return R_nl(n, l, r)
 
 
 def lag_weights_roots(n):
@@ -41,18 +46,20 @@ def calc_g_x(func, x_i):
     return func(x_i) * np.exp(x_i)
 
 
-def q1_func(r, n, m, l):
+def q1_func(r, n, m, l1, l2, q3=False):
     """
     helper function for question 1
     :return: r^2 * R_n_l(r) * R_m_l(r)
     """
-    return (r ** 2) * calc_R_nl(r, n, l) * calc_R_nl(r, m, l)
+    ret = (r ** 2) * calc_R_nl(r, n, l1) * calc_R_nl(r, m, l1) if not q3 else (r ** 2) * calc_R_nl(r, n, l1) * (
+            1 / r) * calc_R_nl(r, m, l2)
+    return ret
 
 
-def integrate_q1(num_roots, n, m, l, func=q1_func):
+def integrate_q1(num_roots, n, m, l1, l2, q3=False, func=q1_func):
     """
     integrate over given function using gaussian quadrature
-    :param l:
+    :param l1:
     :param m:
     :param n:
     :param num_roots:
@@ -63,7 +70,7 @@ def integrate_q1(num_roots, n, m, l, func=q1_func):
     x_i_arr, w_i_arr = lag_weights_roots(num_roots)
     for i in range(num_roots):
         x_i = x_i_arr[i]
-        f_x = func(x_i, n, m, l) * np.exp(float(x_i))
+        f_x = func(x_i, n, m, l1, l2, q3) * np.exp(float(x_i))
         sum += w_i_arr[i] * f_x
     return sum
 
@@ -76,15 +83,21 @@ def deriv_R_nl(r, n, l):
     :param l:
     :return:
     """
-    rho, poly_deg = float(2 * r), n - l - 1
-    in_sqrt = 8 * ((np.math.factorial(poly_deg)) / (np.math.factorial(n + l + 1)))
-    outer1 = l * (rho ** (l - 1) * L(poly_deg, 2 * l + 2)(np.around(rho, 20))) * np.exp(-rho / 2)
-    outer2 = (rho ** l) * L(poly_deg - 1, 2 * l + 3)(np.around(rho, 20)) * np.exp(-rho / 2)
-    outer3 = (rho ** l) * L(poly_deg, 2 * l + 2)(np.around(rho, 20)) * np.exp(-rho / 2)
-    return in_sqrt * (outer1 - outer2 - outer3)
+
+    # rho, poly_deg = np.around(float(2 * r), 20), n - l - 1
+    # in_sqrt = 8 * ((np.math.factorial(poly_deg)) / (np.math.factorial(n + l + 1)))
+    # outer1 = 2 * l * (rho ** (l - 1) * L(poly_deg, 2 * l + 2)(rho)) * np.exp(-rho / 2)
+    # outer2 = (rho ** l) * np.exp(-rho / 2) if n - l - 2 < 0 else (rho ** l) * L(poly_deg - 1, 2 * l + 3)(rho) * np.exp(
+    #     -rho / 2)
+    # outer3 = (rho ** l) * L(poly_deg, 2 * l + 2)(rho) * np.exp(-rho / 2)
+    # return np.sqrt(in_sqrt) * (outer1 - 2 * outer2 - outer3)
+    def f(x):
+        return R_nl(n, l, x)
+
+    return derivative(f, r, dx=1e-7)
 
 
-def integrate_q2(r, n1, n2, l1, l2, num_roots):
+def integrate_q2(n1, n2, l1, l2, num_roots):
     """
     calculates the kinetic energy using numerical evaluation of the integral
     """
@@ -93,22 +106,66 @@ def integrate_q2(r, n1, n2, l1, l2, num_roots):
     x_i_arr, w_i_arr = lag_weights_roots(num_roots)
     for i in range(num_roots):
         x_i = x_i_arr[i]
-        integral1 += w_i_arr[i] * (r**2) * deriv_R_nl(r, n1, l1) * deriv_R_nl(r, n2, l2) * np.exp(float(x_i))
-        integral2 += w_i_arr[i] * calc_R_nl(r, n1, l1) * calc_R_nl(r, n2, l2) * np.exp(float(x_i))
-    return delta*(0.5 * integral1 + 0.5 * l1 * (l1 + 1) * integral2)
+        integral1 += w_i_arr[i] * (x_i ** 2) * deriv_R_nl(x_i, n1, l1) * deriv_R_nl(x_i, n2, l2) * np.exp(float(x_i))
+        integral2 += w_i_arr[i] * calc_R_nl(x_i, n1, l1) * calc_R_nl(x_i, n2, l2) * np.exp(float(x_i))
+    return delta * (0.5 * integral1 + 0.5 * l1 * (l1 + 1) * integral2)
+
+
+def integrate_q3(n, m, l1, l2, num_roots):
+    """
+    numerically evaluates the matrix elements of the electrical potential
+    :return:
+    """
+    delta = 1 if l1 == l2 else 0
+    return - delta * integrate_q1(num_roots, n, m, l1, l2, True)
+
+
+def calc_hamiltonian(N=21):
+    """
+    calculates the hamiltonian matrix
+    :param N: int - size of hamiltonian will be NxN
+    """
+    H = np.zeros((N, N))
+    for i in range(1, N):
+        for j in range(1, N):
+            H[i, j] = calc_H_cell(i, j)
+
+    # diagonlize
+    w, v = LA.eig(H[1:, 1:])  # ignore first row and col
+    return w, v
+
+
+def calc_H_cell(n1, n2, num_roots=20, l=0, ignore_B=True):
+    """
+
+    :param n1:
+    :param n2:
+    :param num_roots:
+    :param l:
+    :param ignore_B:
+    :return:
+    """
+    T_n1n2 = integrate_q2(n1, n2, l, l, num_roots)
+    C_n1n2 = integrate_q3(n1, n2, l, l, num_roots)
+    return T_n1n2 + C_n1n2 if ignore_B else 0  # TODO: handle case with magnetic field
 
 
 if __name__ == '__main__':
     x_arr = [i for i in range(1, 21)]
-    n_eq_m_arr, n_neq_m_arr = [], []
-    for x in x_arr:
-        n_eq_m_arr.append(integrate_q1(x, 3, 3, 1))
-        n_neq_m_arr.append(integrate_q1(x, 3, 2, 1))
-    fig, (ax1, ax2) = plt.subplots(nrows=2, sharex=True)
-    plt.xlabel("# of roots")
-    plt.subplots_adjust(hspace=.0)
-    ax1.set_title(r'$\int r^{2}*R_{nl}*R_{ml}$ vs # of roots')
-    ax1.scatter(x_arr, n_eq_m_arr, c='firebrick', label=r'n=m'), ax1.legend(), ax1.grid()
-    ax2.scatter(x_arr, n_neq_m_arr, c='navy', label=r'$n \neq m$'), ax2.legend(), ax2.grid()
-    plt.savefig('Q1 figure')
-    plt.show()
+    # n_eq_m_arr, n_neq_m_arr = [], []
+    # for x in x_arr:
+    #     n_eq_m_arr.append(integrate_q1(x, 3, 3, 1, 1))
+    #     n_neq_m_arr.append(integrate_q1(x, 3, 2, 1, 1))
+    # fig, (ax1, ax2) = plt.subplots(nrows=2, sharex=True)
+    # plt.xlabel("# of roots")
+    # plt.subplots_adjust(hspace=.0)
+    # ax1.set_title(r'$\int r^{2}*R_{nl}*R_{ml}$ vs # of roots')
+    # ax1.scatter(x_arr, n_eq_m_arr, c='firebrick', label=r'n=m'), ax1.legend(), ax1.grid()
+    # ax2.scatter(x_arr, n_neq_m_arr, c='navy', label=r'$n \neq m$'), ax2.legend(), ax2.grid()
+    # # plt.savefig('Q1 figure')
+    # plt.show()
+    w, v = calc_hamiltonian()
+    # print(np.diag(w))
+
+    plt.scatter(x_arr, [E_nl(i) for i in range(1, 21)], alpha=0.5, c='hotpink', label='Ideal'), plt.xlabel(r'n'), plt.ylabel(r'$E_n$')
+    plt.scatter(x_arr, w, c='navy', alpha=0.5, label='Numeric'), plt.legend(), plt.title(r'$E_n$ vs. n'), plt.show()
